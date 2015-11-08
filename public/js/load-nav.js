3a28c9aca0945ee773c056012298b0d58ab8ee78
yuxin <cys32524@gmail.com>
2015-11-08 17:56:17 -0500

    initial commit use bootstrap tap template

---
+++ public/js/load-nav.js
@@ -0,0 +1,8 @@
+$(document).ready( function() {
+
+	// load navigation bar when the page is ready
+    $("#nav").load("nav",function(){
+    	// load the javascript for navbar and toggle effect
+	});
+    
+});
